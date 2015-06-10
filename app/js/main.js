fa2712392412b6d33ddb7505dd7a7d4a59f4664a
julianbauer <julian@overnice.de>
2015-06-10 21:27:45 +0200

    final changes

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
+	
 	$('.mailing-list-input').focus(function() {
 		$('.mailing-list-input-container').addClass('has-submit-button');
+	});
+	
+	$('#imprint-link').click(function() {
+		$('#imprint').addClass('is-visible');
+		$('html, body').animate({
+        scrollTop: $("#imprint").offset().top
+    }, 500);
+		$(this).delay(500).addClass('is-hidden');
 	});
 });
